675461012fc9b5d47747ab76894c46327d688c8b
Богдан <bogdan@MacBook-Air-Bogdan.local>
2026-05-12 18:03:19 +0700

    add second introdution screen

--- lib/src/core/theme/app_colors.dart
+++ lib/src/core/theme/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color background = Color(0xff0D0D0D);
+  static const Color background = Color(0xff000000);
   static const Color primary = Color(0xFFC0392B); // Красный/Оранжевый
   static const Color accent = Color(0xFFD35400); // Оранжево-коричневый
   static const Color textWhite = Colors.white;
--- lib/src/features/onboarding/presentation/introduction_screen_1.dart
+++ lib/src/features/onboarding/presentation/introduction_screen_1.dart
@@ -7,11 +7,20 @@ import '../../../core/theme/app_colors.dart';
 import '../../../core/theme/app_text_styles.dart';
 import '../../../core/widgets/primary_button.dart';
 
-class IntroductionScreen1 extends ConsumerWidget {
+
+class IntroductionScreen1 extends ConsumerStatefulWidget {
   const IntroductionScreen1({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<IntroductionScreen1> createState() => _IntroductionScreen1State();
+}
+
+class _IntroductionScreen1State extends ConsumerState<IntroductionScreen1> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+
+  @override
+  Widget build(BuildContext context) {
     final pageDecoration = PageDecoration(
       titleTextStyle: AppTextStyles.onboardingTitle,
       bodyTextStyle: AppTextStyles.onboardingBody,
@@ -24,6 +33,7 @@ class IntroductionScreen1 extends ConsumerWidget {
     );
 
     return IntroductionScreen(
+      key: introKey,
       globalBackgroundColor: AppColors.background,
       allowImplicitScrolling: true,
       globalHeader:  SafeArea(
@@ -52,14 +62,42 @@ class IntroductionScreen1 extends ConsumerWidget {
           ),
           decoration: pageDecoration,
         ),
+        PageViewModel(
+          title: "Фокус на главном",
+          body: "Контролируйте сроки и не упускайте важное",
+          image: Stack(
+            children: [
+              Image.asset(
+                'assets/images/onboarding_bg_2.png',
+                fit: BoxFit.cover,
+                height: double.infinity,
+                width: double.infinity,
+                alignment: Alignment.center,
+              ),
+            ],
+          ),
+          decoration: pageDecoration.copyWith(
+            fullScreen: true,
+            pageColor: Colors.transparent,
+          ),
+        ),
       ],
       onDone: () => ref.read(onboardingProvider.notifier).completeOnboarding(),
       onSkip: () => ref.read(onboardingProvider.notifier).completeOnboarding(),
       showSkipButton: false,
-      showBackButton: false,
-      next: const Icon(Icons.arrow_forward, color: AppColors.textWhite),
-      done: PrimaryButton(
+      showBackButton: true,
+      back: Text(
+        'Назад',
+        style: AppTextStyles.buttonText.copyWith(
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+      next: PrimaryButton(
         text: 'Далее',
+        onPressed: () => introKey.currentState?.next(),
+      ),
+      done: PrimaryButton(
+        text: 'Готово',
         onPressed: () => ref.read(onboardingProvider.notifier).completeOnboarding(),
       ),
       dotsDecorator: DotsDecorator(
